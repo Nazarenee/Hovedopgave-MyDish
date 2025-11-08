bb03ff84f781028c5c7654dd54a24aacccbc1dd4
Nazarenee <christianglindemannmeggele@gmail.com>
2025-11-08 14:50:38 +0100

    entities startup

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,24 @@
+package entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    @Column(nullable = false)
+    private String userName;
+    @Column(nullable = false)
+    private String password;
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    private List<Recipe> recipes = new ArrayList<>();
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Comment> comments = new ArrayList<>();
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Like> likes = new ArrayList<>();;
+}
