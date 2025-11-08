163943a3179fbcbdd8bf72d0a867d71cf2ef72e2
Nazarenee <christianglindemannmeggele@gmail.com>
2025-11-08 15:25:13 +0100

    initial repositories

---
+++ src/main/java/Repository/RecipeRepository.java
@@ -0,0 +1,7 @@
+package Repository;
+
+import entities.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+}
---
+++ src/main/java/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package Repository;
+
+import entities.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<Recipe, Long> {
+    List<Recipe> findByAuthorUserId(Long authorId);
+}
