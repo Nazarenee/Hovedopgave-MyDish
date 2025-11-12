706b152eece360929e59016bd23afce01853b311
Nazarenee <christianglindemannmeggele@gmail.com>
2025-11-12 14:00:20 +0100

    forgot @Service

--- src/main/java/com/example/demo/services/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/services/CustomUserDetailsService.java
@@ -1,10 +1,15 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.User;
 import com.example.demo.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
@@ -14,6 +19,11 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUserName(username).orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+        User user =  userRepository.findByUserName(username).orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUserName())
+                .password(user.getPassword())
+                .authorities(new ArrayList<>())
+                .build();
     }
 }
