19eb26d8140a53134c65bbdd71c6fc85cfb2faf0
Nazarenee <christianglindemannmeggele@gmail.com>
2025-11-13 13:43:10 +0100

    added regex on dto directly for validation

--- src/main/java/com/example/demo/DTO/RegisterRequest.java
+++ src/main/java/com/example/demo/DTO/RegisterRequest.java
@@ -1,10 +1,20 @@
 package com.example.demo.DTO;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public class RegisterRequest {
     @JsonProperty("username")
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String userName;
+    @NotBlank(message = "Password is required")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+            message = "Password must be at least 8 characters and contain 1 uppercase , 1 lowercase, and 1 digit"
+    )
     private String password;
     private String email;
 
