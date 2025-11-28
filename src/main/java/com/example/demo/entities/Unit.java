73636d5e5d69528cb24a65385272875ffdef7790
Nazarenee <cph-cm425@stud.ek.dk>
2025-11-28 15:37:42 +0100

    deleted nonusued units

--- src/main/java/com/example/demo/entities/Unit.java
+++ src/main/java/com/example/demo/entities/Unit.java
@@ -5,21 +5,12 @@ public enum Unit {
     KILOGRAM("kg"),
     OUNCE("oz"),
     POUND("lb"),
-
     MILLILITER("ml"),
     LITER("l"),
     DECILITER("dl"),
     TEASPOON("tsp"),
     TABLESPOON("tbsp"),
-    CUP("cup"),
-    FLUID_OUNCE("fl oz"),
-    PINT("pint"),
-    QUART("quart"),
-    GALLON("gallon"),
-
-    PIECE("piece"),
-    PINCH("pinch"),
-    TO_TASTE("to taste");
+    GALLON("gallon");
 
     private final String selectedUnit;
 
